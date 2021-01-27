afbbda0865b30d9a286b54c225940f8f57c4dbdd
Brian Sunday <brians931@gmail.com>
2021-01-27 12:55:44 +0300

    initial migrations of tables without fk ran

--- db/migrations/20210127115451_tables_withoutFk.js
+++ db/migrations/20210127115451_tables_withoutFk.js
@@ -9,9 +9,68 @@ const {
  * @param {import('knex')} knex
  */
 
-exports.up = function (knex) {};
+exports.up = async function (knex) {
+    // starting with tables without fk references
+    await Promise.all([
+        knex.schema.createTable(tableNames.user, (table) => {
+            addDefaultColumnsUser(table);
+            addDefaultColumns(table);
+        }),
+        knex.schema.createTable(tableNames.subscription_type, (table) => {
+            table.increments().notNullable();
+            table.string("subcription_type_name", 25).notNullable();
+            table.integer("amount").notNullable();
+            table.enum("subcription_duration", [
+                "Day",
+                "Week",
+                "Month",
+                "Year",
+            ]);
+            table.integer("duration_in_days");
+            addDefaultColumns(table);
+        }),
+        knex.schema.createTable(tableNames.job_status, (table) => {
+            table.increments().notNullable();
+            table.string("job_status_name", 25).notNullable();
+            addDefaultColumns(table);
+        }),
+        knex.schema.createTable(tableNames.industry, (table) => {
+            table.increments().notNullable();
+            table.string("industry_name", 100).notNullable();
+            addDefaultColumns(table);
+        }),
+        knex.schema.createTable(tableNames.skill, (table) => {
+            table.increments().notNullable();
+            table.string("skill_name", 50).notNullable();
+            addDefaultColumns(table);
+        }),
+        knex.schema.createTable(tableNames.proposal_status, (table) => {
+            table.increments().notNullable();
+            table.string("proposal_status_name", 50).notNullable();
+            addDefaultColumns(table);
+        }),
+        knex.schema.createTable(tableNames.payment_type, (table) => {
+            table.increments().notNullable();
+            table.string("payment_type_name", 50).notNullable();
+            addDefaultColumns(table);
+        }),
+    ]);
+};
 
 /**
  * @param {import('knex')} knex
  */
-exports.down = function (knex) {};
+exports.down = async function (knex) {
+    // drop in any order since they are independent
+    await Promise.all(
+        [
+            tableNames.user,
+            tableNames.subscription_type,
+            tableNames.job_status,
+            tableNames.industry,
+            tableNames.skill,
+            tableNames.proposal_status,
+            tableNames.payment_type,
+        ].map((tableName) => knex.schema.dropSchemaIfExists(tableName))
+    );
+};
