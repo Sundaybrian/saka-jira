da1baf8157ea0cecd48909fb91b9cc4fdf8cfc36
Brian Sunday <brians931@gmail.com>
2021-01-28 01:14:25 +0300

    exporting validators correctly

--- src/api/auth/auth.validators.ts
+++ src/api/auth/auth.validators.ts
@@ -2,34 +2,34 @@ import { Request, Response, NextFunction } from "express";
 import Joi from "joi";
 import validateRequest from "../../_middlewares/validateRequest";
 
-exports.signupSchema = (req: Request, res: Response, next: NextFunction) => {
+export function signupSchema(req: Request, res: Response, next: NextFunction) {
     const schema = Joi.object({
-        firstName: Joi.string().required(),
-        lastName: Joi.string().required(),
-        phoneNumber: Joi.string().min(10).max(15).required(),
+        first_name: Joi.string().required(),
+        last_name: Joi.string().required(),
+        phone_number: Joi.string().min(10).max(15).required(),
         // role: Joi.string().min(5).required(),
         email: Joi.string().email().required(),
         password: Joi.string().min(8).required(),
-        confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
+        // confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
     });
     validateRequest(req, next, schema);
-};
+}
 
-exports.signinSchema = (req: Request, res: Response, next: NextFunction) => {
+export function signinSchema(req: Request, res: Response, next: NextFunction) {
     const schema = Joi.object({
         email: Joi.string().email().required(),
         password: Joi.string().min(8).required(),
     });
     validateRequest(req, next, schema);
-};
+}
 
-exports.updateSchema = (req: Request, res: Response, next: NextFunction) => {
+export function updateSchema(req: Request, res: Response, next: NextFunction) {
     const schemaRules = {
-        firstName: Joi.string().empty(""),
-        lastName: Joi.string().empty(""),
+        first_name: Joi.string().empty(""),
+        last_name: Joi.string().empty(""),
         email: Joi.string().email().empty(""),
         role: Joi.string().min(5).empty(""),
-        phoneNumber: Joi.string().min(10).max(15).empty(""),
+        phone_number: Joi.string().min(10).max(15).empty(""),
         password: Joi.string().min(8).empty(""),
         confirmPassword: Joi.string().valid(Joi.ref("password")).empty(""),
     };
@@ -38,15 +38,15 @@ exports.updateSchema = (req: Request, res: Response, next: NextFunction) => {
     const schema = Joi.object(schemaRules).with("password", "confirmPassword");
 
     validateRequest(req, next, schema);
-};
+}
 
-exports.verifyEmailSchema = (
+export function verifyEmailSchema(
     req: Request,
     res: Response,
     next: NextFunction
-) => {
+) {
     const schema = Joi.object({
         token: Joi.string().required(),
     });
     validateRequest(req, next, schema);
-};
+}
