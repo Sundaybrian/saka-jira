7df1b53f50868f5c1f84e027eddc21ba0259ed1a
Brian Sunday <brians931@gmail.com>
2021-01-28 00:15:47 +0300

    add email util

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,19 @@
-const dotenv = require("dotenv");
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export default {
     port: process.env.PORT,
     databaseURL: process.env.DATABASE_URL,
+    /**
+     * Your secret sauce
+     */
     JWT_SECRET: process.env.JWT_SECRET,
-    mailGun: {},
+    /**
+     * Mailgun email credentials
+     */
+    emails: {
+        apiKey: process.env.MAILGUN_API_KEY,
+        domain: process.env.MAILGUN_DOMAIN,
+    },
 };
