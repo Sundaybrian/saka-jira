5dac3978095e38852c70cb6c23a7a525bfabf935
Brian Sunday <brians931@gmail.com>
2021-01-29 02:03:39 +0300

    skill and schema

---
+++ src/models/Skill/Skill.Model.js
@@ -0,0 +1,17 @@
+const { Model } = require("objection");
+
+const tableNames = require("../../constants/tableNames");
+// const db = require("../../db");
+const schema = require("./skill.schema.json");
+
+class Skill extends Model {
+    static get tableName() {
+        return tableNames.skill;
+    }
+
+    static get jsonSchema() {
+        return schema;
+    }
+}
+
+module.exports = Skill;
