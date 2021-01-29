236b021aa122bbb65b1947b7e9822e1e3a58cf3c
Brian Sunday <brians931@gmail.com>
2021-01-29 04:02:22 +0300

    payment type model

---
+++ src/models/PaymentType/PaymentType.Model.js
@@ -0,0 +1,17 @@
+const { Model } = require("objection");
+
+const tableNames = require("../../constants/tableNames");
+// const db = require("../../db");
+const schema = require("./paymentType.schema.json");
+
+class PaymentType extends Model {
+    static get tableName() {
+        return tableNames.payment_type;
+    }
+
+    static get jsonSchema() {
+        return schema;
+    }
+}
+
+module.exports = PaymentType;
