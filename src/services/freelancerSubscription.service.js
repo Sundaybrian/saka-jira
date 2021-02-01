1894b5b40a7b59798fe7c4c0c0c6e0a464568b02
Brian Sunday <brians931@gmail.com>
2021-02-01 13:54:37 +0300

    add freelancerSubscription service

--- src/services/freelancerSubscription.service.js
+++ src/services/freelancerSubscription.service.js
@@ -1,15 +1,16 @@
 const FreelancerSubscription = require("../models/FreelancerSubscription/FreelancerSubscription.Model");
-const tableNames = require("../constants/tableNames");
-import DateService from "./date.service";
 
 class FreelancerSubscriptionService {
     constructor() {}
 
-    static async createFreelancerSubscription(freelancer_id) {
-        // technically wont be used either
+    static async createFreelancerSubscription(freelancer_id, expiry_date) {
+        // technically wont be used directly,
+        // edge case will be when a manual creation is required!
+
         try {
             const subscription = await FreelancerSubscription.query().insert({
                 freelancer_id,
+                expiry_date,
             });
 
             return subscription;
@@ -24,34 +25,18 @@ class FreelancerSubscriptionService {
         return subscriptions;
     }
 
-    static async updateFreelancerSubscription(id, number_of_days) {
-        // technically will be run by payment sytem
+    static async updateFreelancerSubscription(freelancer_id, expiry_date) {
+        // technically will be run by payment sytem service
         try {
-            // get current expiry date
-            const subscription = await this.getFreelancerSubscription(id);
+            const updatedSubscription = await FreelancerSubscription.query()
+                .patch({
+                    expiry_date,
+                })
+                .where({ freelancer_id })
+                .returning("*")
+                .first();
 
-            // check if current date is greater than expiry date
-            const expired = await DateService.isExpired(
-                subscription.expiry_date
-            );
-
-            let new_expiry_date;
-
-            if (expired) {
-                new_expiry_date = DateService.overWriteDate(number_of_days);
-            } else {
-                new_expiry_date = DateService.topUpDate(
-                    subscription.expiry_date,
-                    number_of_days
-                );
-            }
-
-            const updatedSubscription = await subscription
-                .$query()
-                .patchAndFetch({
-                    expiry_date: new_expiry_date,
-                });
-            return updatedSubscription;
+            return this.basicDetails(updatedSubscription);
         } catch (error) {
             throw error;
         }
@@ -73,13 +58,16 @@ class FreelancerSubscriptionService {
         }
     }
 
+    // TODO why would we want to delete a subscription??
     static async _delete(freelancer_id) {
-        const freelancer = await this.getFreelancerSubscription(freelancer_id);
-        if (!freelancer) {
+        const freelancerSubscription = await this.getFreelancerSubscription(
+            freelancer_id
+        );
+        if (!freelancerSubscription) {
             return null;
         }
 
-        await FreelancerSubscription.query().deleteById(freelancer_id);
+        await FreelancerSubscription.query().delete.where({ freelancer_id });
 
         return true;
     }
@@ -103,4 +91,4 @@ class FreelancerSubscriptionService {
     }
 }
 
-module.exports = FreelancerSubscriptionSubscriptionService;
+module.exports = FreelancerSubscriptionService;
