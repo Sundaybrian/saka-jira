216e553f22ba8dc72b30efd70c3a3d62cf27d739
Brian Sunday <brians931@gmail.com>
2021-02-03 20:01:01 +0300

    fix perm method on hiring manager

--- src/utils/_permissions/hiringManager.js
+++ src/utils/_permissions/hiringManager.js
@@ -1,9 +1,31 @@
 const Role = require("../../constants/roles");
+const HiringManagerService = require("../../services/hiringManager.service");
 
-function canUpdateHiringManager(user, hiring_manager) {
-    return user.role == Role.admin || user.id == hiring_manager.user_id;
+// permissions
+// function canUpdateHiringManager(user, hiring_manager) {
+//     return user.role == Role.admin || user.id == hiring_manager.user_id;
+// }
+
+// function canCreateJob(user, hiring_manager) {
+//     return user.role == Role.admin || user.id == freelancer.user_id;
+// }
+
+
+// permission middlewares
+function setHiringManager(req, res, next) {
+    HiringManagerService.fetchHiringManager({user_id: parseInt(req.user.id)})
+        .then((hiringManager) => {
+            req.hiringManager = hiringManager;
+            next();
+        })
+        .catch(next);
 }
 
+
+
+
+// module export
 module.exports = {
-    canUpdateHiringManager,
-};
+    setHiringManager
+    };
+
