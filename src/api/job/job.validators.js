ab6e62879111a38b907854b917c21baf7a421d3a
Brian Sunday <brians931@gmail.com>
2021-02-03 19:06:28 +0300

    job validators

---
+++ src/api/job/job.validators.js
@@ -0,0 +1,37 @@
+const Joi = require("joi");
+const validateRequest = require("../../_middlewares/validateRequest");
+
+exports.createSchema = (req, res, next) => {
+    const schema = Joi.object({
+        title: Joi.string().max(30).required(),
+        description: Joi.string().max(1000).required(),
+        main_skill: Joi.string().max(100),
+        // job_status_id: Joi.number().integer().required(),
+        industry_id: Joi.number().integer().required(),
+        budget_range_min: Joi.number().integer(),
+        start_date: Joi.date().iso().required().raw(),
+        end_date: Joi.date().iso().required().raw(),
+        budget_range_max: Joi.number().integer(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+    });
+    validateRequest(req, next, schema);
+};
+
+exports.updateSchema = (req, res, next) => {
+    const schema = Joi.object({
+        title: Joi.string().max(30).empty(""),
+        description: Joi.string().max(1000).empty(""),
+        main_skill: Joi.string().max(100),
+        job_status_id: Joi.number().integer().empty(""),
+        industry_id: Joi.number().integer().empty(""),
+        budget_range_min: Joi.number().integer(),
+        start_date: Joi.date().iso().empty("").raw(),
+        end_date: Joi.date().iso().empty("").raw(),
+        budget_range_max: Joi.number().integer(),
+        latitude: Joi.number().empty(""),
+        longitude: Joi.number().empty(""),
+    });
+
+    validateRequest(req, next, schema);
+};
