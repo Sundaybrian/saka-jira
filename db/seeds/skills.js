4183299d1e29d2a0d9221aedd7475a6b9b79ba01
Brian Sunday <brians931@gmail.com>
2021-02-04 03:01:46 +0300

    mount job routes and seed job status

--- db/seeds/skills.js
+++ db/seeds/skills.js
@@ -3,13 +3,15 @@ const tableNames = require("../../src/constants/tableNames");
 
 exports.seed = async function (knex) {
     
-   const skills = [
-     {skill_name: "Plumbing"},
-     {skill_name:"Python"}
-   ]
+   const insertedSkills = await knex(tableNames.skill).insert({skill_name: "Plumbing"},
+   {skill_name:"Python"});
 
-    
-   const insertedSkills = await knex(tableNames.skill).insert(skills);
+
+   const job_statuses = await knex(tableNames.job_status).insert([
+     {job_status_name: "accepting bids"},
+     {job_status_name: "closed"},
+     {job_status_name: "inprogress"}
+   ])
 
     
 };
