5114dd2d61e4ac8202749b7125fd19140a0d798d
Brian Sunday <brians931@gmail.com>
2021-02-12 16:54:20 +0300

    inserting seeds in order and add refactored proposal history enum colums

--- db/migrations/20210212100138_upgrade_proposal_history.js
+++ db/migrations/20210212100138_upgrade_proposal_history.js
@@ -1,25 +1,22 @@
 const tableNames = require("../../src/constants/tableNames");
 
-const {
-    references,
-    url,
-} = require("../../src/utils/tableUtils");
+const { references, url } = require("../../src/utils/tableUtils");
 
-exports.up = async function(knex) {
+exports.up = async function (knex) {
     await knex.schema.table(tableNames.proposal_history, (table) => {
         references(table, tableNames.job, null, true);
         references(table, tableNames.freelancer, null, true).index();
-        table.string("client_comment", 1500);
-        table.enum("client_rating", [1, 2, 3, 4, 5]);
-        table.string("freelancer_comment", 1500);
-        table.enum("freelancer_rating", [1, 2, 3, 4, 5]);
+        table.integer("client_rating").defaultTo(1);
+        table.string("client_comment", 1500).defaultTo("no comment");
+        table.string("freelancer_comment", 1500).defaultTo("no comment");
+        table.integer("freelancer_rating").defaultTo(1);
         url(table, "website_url");
         table.integer("payment_amount");
         // references(table, tableNames.company, null, false);
     });
 };
 
-exports.down = function(knex) {
+exports.down = async function (knex) {
     await knex.schema.table(tableNames.proposal_history, (table) => {
         table.dropColumn("job_id");
         table.dropColumn("freelancer_id");
