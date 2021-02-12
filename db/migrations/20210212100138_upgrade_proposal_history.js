8cdbf1e3ce18aa712250f916bd5640edcfe2551b
Brian Sunday <brians931@gmail.com>
2021-02-12 10:25:19 +0300

    add more columns to proposal_history

---
+++ db/migrations/20210212100138_upgrade_proposal_history.js
@@ -0,0 +1,35 @@
+const tableNames = require("../../src/constants/tableNames");
+
+const {
+    addDefaultColumns,
+    addDefaultGeoLocations,
+    references,
+    url,
+} = require("../../src/utils/tableUtils");
+
+exports.up = async function(knex) {
+    await knex.schema.table(tableNames.proposal_history, (table) => {
+        references(table, tableNames.job, null, true);
+        references(table, tableNames.freelancer, null, true).index();
+        table.string("client_comment", 1500);
+        table.enum("client_rating", [1, 2, 3, 4, 5]);
+        table.string("freelancer_comment", 1500);
+        table.enum("freelancer_rating", [1, 2, 3, 4, 5]);
+        url(table, "website_url");
+        table.integer("payment_amount");
+        // references(table, tableNames.company, null, false);
+    });
+};
+
+exports.down = function(knex) {
+    await knex.schema.table(tableNames.proposal_history, (table) => {
+        table.dropColumn("job_id");
+        table.dropColumn("freelancer_id");
+        table.dropColumn("client_comment");
+        table.dropColumn("client_rating");
+        table.dropColumn("freelancer_comment");
+        table.dropColumn("freelancer_rating");
+        table.dropColumn("website_url");
+        table.dropColumn("payment_amount");
+    });
+};
