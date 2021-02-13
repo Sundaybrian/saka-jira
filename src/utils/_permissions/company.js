a61bcbd62105c0ea67d2227dd6fd03ff5a8e98f6
Brian Sunday <brians931@gmail.com>
2021-02-13 19:49:47 +0300

    company delete permissions

--- src/utils/_permissions/company.js
+++ src/utils/_permissions/company.js
@@ -6,6 +6,11 @@ function canUpdateCompany(user, company) {
     return user.role == Role.admin || user.id == company.owner_id;
 }
 
+// permissions
+function canDeleteCompany(user, company) {
+    return user.role == Role.admin || user.id == company.owner_id;
+}
+
 // permission middlewares
 function setCompanyOwner(req, res, next) {
     const id = parseInt(req.params.id);
@@ -27,8 +32,18 @@ function authUpdateCompany(req, res, next) {
     next();
 }
 
+function authDeleteCompany(req, res, next) {
+    if (!canDeleteCompany(req.user, req.company)) {
+        res.status(401);
+        return res.send("Action is Not Allowed");
+    }
+
+    next();
+}
+
 // module export
 module.exports = {
     setCompanyOwner,
     authUpdateCompany,
+    authDeleteCompany,
 };
