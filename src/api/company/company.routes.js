17b1000b6bfa1b7378709bec5813c3165abe6340
Brian Sunday <brians931@gmail.com>
2021-02-13 19:25:06 +0300

    company create and get all routes

---
+++ src/api/company/company.routes.js
@@ -0,0 +1,92 @@
+const router = require("express").Router();
+const { Auth } = require("../../_middlewares/auth");
+const Role = require("../../constants/roles");
+const { createSchema, updateSchema } = require("./company.validators");
+const {
+    setOwner,
+    authUpdateOwnerCompany,
+} = require("../../utils/_permissions/company");
+
+const CompanyService = require("../../services/company.service");
+
+router.post("/", Auth([Role.user]), createSchema, create);
+router.get("/", Auth([Role.admin]), getAllCompanies);
+// router.get("/mine", Auth([Role.user]), getMyCompanies);
+router.get("/:id", Auth(), getCompanyById);
+router.patch("/:id", Auth([Role.user]), updateSchema, update);
+router.delete("/:id", Auth([Role.admin, Role.user]), _deleteCompany);
+
+module.exports = router;
+
+function create(req, res, next) {
+    req.body.owner_id = req.user.id;
+    CompanyService.create(req.body)
+        .then((company) => res.json(company))
+        .catch(next);
+}
+
+function getAllCompanies(req, res, next) {
+    let nextPage = null;
+
+    const limit = parseInt(req.query.limit) || 30;
+
+    const match = {
+        active: true,
+    };
+
+    if (req.query.nextPage) {
+        nextPage = req.query.nextPage;
+    }
+
+    CompanyService.getAllCompanies(nextPage, match, limit)
+        .then((companies) => {
+            return companies ? res.json(companies) : res.sendStatus(404);
+        })
+        .catch(next);
+}
+
+// function getMyCompanies(req, res, next) {
+//     CompanyService.getMyCompanies(req.user.id)
+//         .then((companies) =>
+//             companies ? res.json(companies) : res.sendStatus(404)
+//         )
+//         .catch(next);
+// }
+
+function getCompanyById(req, res, next) {
+    // owner can get his company and the admin can get any company
+    CompanyService.getCompanyById(req.params.id)
+        .then((company) => {
+            if (
+                company.owner_id !== req.user.id &&
+                req.user.role !== Role.admin
+            ) {
+                return res.status(401).json({ message: "Unauthorized" });
+            }
+
+            res.json(company);
+        })
+        .catch(next);
+}
+
+function update(req, res, next) {
+    // only owner can update their company
+    CompanyService.updateCompany(
+        { id: req.params.id, owner_id: req.user.id },
+        req.body
+    )
+        .then((company) => res.json(company))
+        .catch(next);
+}
+
+function _deleteCompany(req, res, next) {
+    // only owner delete can delete their company
+    CompanyService._delete({
+        id: parseInt(req.params.id),
+        owner_id: req.user.id,
+    })
+        .then(() => {
+            res.json({ id: parseInt(req.params.id) });
+        })
+        .catch(next);
+}
