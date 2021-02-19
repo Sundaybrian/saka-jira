a33825a2062eca6089262b896a1101bca96f07c1
Brian Sunday <brians931@gmail.com>
2021-02-19 03:19:59 +0300

    add try catch for send welcome email job

--- src/jobs/sendWelcomeEmail.js
+++ src/jobs/sendWelcomeEmail.js
@@ -3,9 +3,15 @@ const EmailService = require("../services/email.service");
 class SendWelcomeEmail {
 
     async static handler(job, done){
-        const { account, origin}= job.attrs.data;
-        await EmailService.sendVerificationEmail(account, origin);
-        done();
+        try {
+            
+            const { account, origin}= job.attrs.data;
+            await EmailService.sendVerificationEmail(account, origin);
+            done();
+        } catch (error) {
+            done(error);
+
+        }
     }
 }
 
