e28f4a1536e6eb11858ee205299fe351194e9e4a
Brian Sunday <brians931@gmail.com>
2021-02-19 15:11:30 +0300

    fix send welcome email

--- src/jobs/sendWelcomeEmail.js
+++ src/jobs/sendWelcomeEmail.js
@@ -1,18 +1,13 @@
 const EmailService = require("../services/email.service");
 
-class SendWelcomeEmail {
-
-    async static handler(job, done){
-        try {
-            
-            const { account, origin}= job.attrs.data;
-            await EmailService.sendVerificationEmail(account, origin);
-            done();
-        } catch (error) {
-            done(error);
-
-        }
+const SendWelcomeEmail = async (job, done) => {
+    try {
+        const { account, origin } = job.attrs.data;
+        await EmailService.sendVerificationEmail(account, origin);
+        done();
+    } catch (error) {
+        done(error);
     }
-}
+};
 
-module.exports = SendWelcomeEmail
+module.exports = SendWelcomeEmail;
