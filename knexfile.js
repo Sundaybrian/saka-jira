67b1b869b14b2094683eb2580d91a007633a8d97
Brian Sunday <brians931@gmail.com>
2021-02-21 23:53:42 +0300

    setup knex for elastic beanstalk

--- knexfile.js
+++ knexfile.js
@@ -39,11 +39,16 @@ module.exports = {
 
     production: {
         client: "pg",
-        connection: process.env.DATABASE_URL,
-        pool: {
-            min: 2,
-            max: 10,
+        connection: {
+            host: process.env.RDS_HOSTNAME,
+            database: process.env.RDS_DB_NAME,
+            user: process.env.RDS_USERNAME,
+            password: process.env.RDS_PASSWORD,
         },
+        // pool: {
+        //     min: 2,
+        //     max: 10,
+        // },
         migrations: {
             directory: __dirname + "/db/migrations",
         },
