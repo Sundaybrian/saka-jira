0f75823fb12277b4e79028bcdbd7524512d23624
Brian Sunday <brians931@gmail.com>
2021-03-19 09:26:00 +0300

    knex setup for heroku

--- knexfile.js
+++ knexfile.js
@@ -57,26 +57,33 @@ module.exports = {
         },
     },
 
+    // production: {
+    //     client: "pg",
+    //     connection:
+    //         "postgres://uradypostgresu:uradypostgrespass@uradyjobsdbinstance.cl6vfm4enqho.eu-central-1.rds.amazonaws.com:5432/uradyjobsdb",
+
+    //     migrations: {
+    //         directory: __dirname + "/db/migrations",
+    //     },
+
+    //     seeds: {
+    //         directory: __dirname + "/db/seeds/production",
+    //     },
+    // },
+
     production: {
         client: "pg",
-        connection:
-            "postgres://uradypostgresu:uradypostgrespass@uradyjobsdbinstance.cl6vfm4enqho.eu-central-1.rds.amazonaws.com:5432/uradyjobsdb",
-        // connection: {
-        //     host: process.env.RDS_HOSTNAME,
-        //     database: process.env.RDS_DB_NAME,
-        //     user: process.env.RDS_USERNAME,
-        //     password: process.env.RDS_PASSWORD,
-        // },
-        // pool: {
-        //     min: 2,
-        //     max: 10,
-        // },
+        connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + "/db/migrations",
         },
+        pool: {
+            min: 2,
+            max: 10,
+        },
 
         seeds: {
-            directory: __dirname + "/db/seeds/production",
+            directory: __dirname + "/db/seeds",
         },
     },
 };
