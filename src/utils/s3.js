3b969a21662c9e4129933bb7e89c115f608ecbc1
Brian Sunday <brians931@gmail.com>
2021-04-01 15:40:11 +0300

    Add: s3 util file

---
+++ src/utils/s3.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const fs = require("fs");
+const S3 = require("aws-sdk/clients/s3");
+const { promisify } = require("util");
+const mongoose = require("mongoose");
+
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const s3 = new S3({
+    region,
+    accessKeyId,
+    secretAccessKey,
+});
+
+s3.uploadP = promisify(s3.upload);
+
+// uploads single file to s3
+
+function uploadFile(file) {
+    const imageID = new mongoose.Types.ObjectId();
+    const fileStream = fs.createReadStream(file.path);
+
+    console.log(file, imageID);
+
+    const uploadParams = {
+        Bucket: bucketName,
+        Body: fileStream,
+        Key: file.filename,
+    };
+
+    return true;
+    // return s3.uploadP(uploadParams);
+}
+
+module.exports = {
+    uploadFile,
+};
