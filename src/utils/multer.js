1d95856a7a6e23212a82dd4e8255dd6322182ea5
Brian Sunday <brians931@gmail.com>
2021-04-01 15:43:01 +0300

    Add multer util methods

---
+++ src/utils/multer.js
@@ -0,0 +1,19 @@
+const multer = require("multer");
+
+// helpers
+const upload = multer({
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error("please upload an image"));
+        }
+
+        cb(undefined, true);
+    },
+});
+
+modules.export = {
+    upload,
+};
