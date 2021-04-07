325b3c4ffef1f4ad86aca89197882e0167573701
Brian Sunday <brians931@gmail.com>
2021-04-08 02:09:14 +0300

    Update: adding index to token in refreshtoken and bindig to user model

---
+++ db/migrations/20210408015454_add_account_id_refreshToken.js
@@ -0,0 +1,17 @@
+const tableNames = require("../../src/constants/tableNames");
+
+const { references } = require("../../src/utils/tableUtils");
+
+exports.up = async function (knex) {
+    await knex.schema.table(tableNames.refresh_token, (table) => {
+        references(table, tableNames.user, "account", true);
+        table.index("token");
+    });
+};
+
+exports.down = async function (knex) {
+    await knex.schema.table(tableNames.refresh_token, (table) => {
+        table.dropColumn("account_id");
+        table.dropIndex("token");
+    });
+};
