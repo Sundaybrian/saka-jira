3a5ce094ba3f39d92d1df4021b4bab5b21377251
Brian Sunday <brians931@gmail.com>
2021-04-17 18:43:56 +0300

    add nodemailer

--- src/utils/email.js
+++ src/utils/email.js
@@ -6,15 +6,17 @@ const { google } = require("googleapis");
 module.exports = sendEmail;
 
 // These id's and secrets should come from .env file.
-const CLIENT_ID = "YOUR CLIENT ID";
-const CLIENT_SECRET = "YOUR CLIENT SECRET";
+const CLIENT_ID =
+    "926833439120-lvbklo7jbi8t9kaprlsg9hrfdfrbp7g4.apps.googleusercontent.com";
+const CLIENT_SECRET = "usaGOBY_1VxHsMX9FkcRAo6s";
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN = "YOUR REFRESH TOKEN";
+const REFRESH_TOKEN =
+    "1//04TiKIeba50w2CgYIARAAGAQSNwF-L9IrjJjgWmksc4p4S5HHiCeWqR6kL_rHtQNdEBk6CV5cPiftCmnG5N9cSljSVeNwfavjykU";
 
 const oAuth2Client = new google.auth.OAuth2(
     CLIENT_ID,
     CLIENT_SECRET,
-    REDIRECT_URI
+    -REDIRECT_URI
 );
 
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
@@ -34,7 +36,7 @@ async function sendEmail({ to, subject, html, from = "Uraditech@gmail.com" }) {
             service: "gmail",
             auth: {
                 type: "OAuth2",
-                user: "yours authorised email address",
+                user: "uraditech@gmail.com",
                 clientId: CLIENT_ID,
                 clientSecret: CLIENT_SECRET,
                 refreshToken: REFRESH_TOKEN,
