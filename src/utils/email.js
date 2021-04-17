32c9587881ab4c8f8b6a1320fd4c616b94a6aa5a
Brian Sunday <brians931@gmail.com>
2021-04-18 02:14:34 +0300

    Add: new sg toke

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,51 +1,27 @@
 require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
 
-const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
+const apikey =
+    process.env.NODE_ENV == "production"
+        ? "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"
+        : "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";
+
+sgMail.setApiKey(apikey);
 
 module.exports = sendEmail;
 
-// These id's and secrets should come from .env file.
-const CLIENT_ID =
-    "926833439120-lvbklo7jbi8t9kaprlsg9hrfdfrbp7g4.apps.googleusercontent.com";
-const CLIENT_SECRET = "usaGOBY_1VxHsMX9FkcRAo6s";
-const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN =
-    "1//04TiKIeba50w2CgYIARAAGAQSNwF-L9IrjJjgWmksc4p4S5HHiCeWqR6kL_rHtQNdEBk6CV5cPiftCmnG5N9cSljSVeNwfavjykU";
-
-const oAuth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    -REDIRECT_URI
-);
-
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
 async function sendEmail({ to, subject, html, from = "Uraditech@gmail.com" }) {
-    try {
-        const msg = {
-            to,
-            subject,
-            html,
-            from,
-        };
+try {
+    const msg = {
+        to,
+        subject,
+        html,
+        from,
+    };
 
-        const accessToken = await oAuth2Client.getAccessToken();
+    await sgMail.send(msg);
+} catch (error) {
+    throw error
+}
 
-        const transport = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                type: "OAuth2",
-                user: "uraditech@gmail.com",
-                clientId: CLIENT_ID,
-                clientSecret: CLIENT_SECRET,
-                refreshToken: REFRESH_TOKEN,
-                accessToken: accessToken,
-            },
-        });
-
-        await transport.sendMail(msg);
-    } catch (error) {
-        throw error;
-    }
 }
