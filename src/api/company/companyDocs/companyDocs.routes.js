6f9d548f12767d448391aba81c0d4473f43a9a8c
Brian Sunday <brians931@gmail.com>
2021-07-07 18:35:40 +0300

    add companydocs endpoints

--- src/api/company/companyDocs/companyDocs.routes.js
+++ src/api/company/companyDocs/companyDocs.routes.js
@@ -1,11 +1,12 @@
-const { createSchema } = require("./otherSkill.validators");
+const { createSchema } = require("./companyDocs.validators");
 // const Role = require("../../constants/roles");
 const CompanyDocsService = require("../../../services/companyDocs.service");
 const { Auth } = require("../../../_middlewares/auth");
 const {
-    setFreelancer,
-    authUpdateFreelancer,
-} = require("../../../utils/_permissions/freelancer");
+    setCompanyOwner,
+    authUpdateCompany,
+    authDeleteCompany,
+} = require("../../../utils/_permissions/company");
 
 const router = require("express").Router({
     mergeParams: true,
@@ -13,54 +14,52 @@ const router = require("express").Router({
 
 router.post(
     "/",
-    Auth(),
-    setFreelancer,
-    authUpdateFreelancer,
+    Auth([Role.user]),
+    setCompanyOwner,
+    authUpdateCompany,
     createSchema,
-    addSkill
+    addDoc
 );
-router.get("/", Auth(), getMySkills);
-router.delete(
-    "/:skill_id",
-    Auth(),
-    setFreelancer,
-    authUpdateFreelancer,
-    removeSkill
+
+router.get(
+    "/",
+    Auth([Role.user, Role.admin]),
+    setCompanyOwner,
+    authUpdateCompany,
+    getDocs
 );
+
+router.delete("/:doc_id", Auth([Role.admin]), removeDoc);
 
 module.exports = router;
 
-function addSkill(req, res, next) {
-    const { skill_name } = req.body;
+function addDoc(req, res, next) {
     const payload = {
-        skill_name,
-        freelancer_id: parseInt(req.freelancer.id),
+        ...req.body,
+        company_id: parseInt(req.company.id),
     };
 
     CompanyDocsService.addSkill(payload)
-        .then((skill) => res.json(skill))
+        .then((doc) => res.json(doc))
         .catch(next);
 }
 
-function getMySkills(req, res, next) {
-    const freelancer_id = parseInt(req.params.id);
+function getDocs(req, res, next) {
+    const company_id = parseInt(req.params.id);
 
-    CompanyDocsService.getMySkills(freelancer_id)
-        .then((freelancer_and_skills) => {
-            return freelancer_and_skills
-                ? res.json(freelancer_and_skills)
-                : res.sendStatus(404);
+    CompanyDocsService.getDocs(company_id)
+        .then((docs) => {
+            return docs ? res.json(docs) : res.sendStatus(404);
         })
         .catch(next);
 }
 
-function removeSkill(req, res, next) {
+function removeDoc(req, res, next) {
     // only admin can delete /freelancer can unrelate a skill from user
-    const id = parseInt(req.params.skill_id);
-    const freelancer_id = parseInt(req.freelancer.id);
-    CompanyDocsService._removeSkill({ id, freelancer_id })
-        .then((freelancer_skill) => {
-            return !freelancer_skill ? res.sendStatus(404) : res.json({ id });
+    const id = parseInt(req.params.doc_id);
+    CompanyDocsService._removeDoc({ id })
+        .then((doc) => {
+            return !doc ? res.sendStatus(404) : res.json({ id });
         })
         .catch(next);
 }
